b731d1bb082d073749b836f6ec895a2cbd11e128
nurdin-sapariansyah <nurdin27muhammad@gmail.com>
2021-06-09 18:05:14 +0800

    memperbaiki bugs tidak bisa memunculkan craft of plastik & kertas. dan mengganti ArrayList menjadi List

--- app/src/main/java/com/capstoneproject/silimbah/model/DataCraft.kt
+++ app/src/main/java/com/capstoneproject/silimbah/model/DataCraft.kt
@@ -1,17 +1,22 @@
 package com.capstoneproject.silimbah.model
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class DataCraft (
         @SerializedName("id")
+        @Expose
         val id: Int,
 
         @SerializedName("title")
+        @Expose
         val title: String,
 
         @SerializedName("image_url")
+        @Expose
         val image_url : String,
 
         @SerializedName("url")
+        @Expose
         val url: String
 )
--- app/src/main/java/com/capstoneproject/silimbah/model/craftResponse.kt
+++ app/src/main/java/com/capstoneproject/silimbah/model/craftResponse.kt
@@ -1,8 +1,10 @@
 package com.capstoneproject.silimbah.model
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class craftResponse (
     @field:SerializedName("data")
+    @Expose
     val data: List<DataCraft>
         )
